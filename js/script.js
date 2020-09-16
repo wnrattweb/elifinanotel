833b65eed272ae970d8967f6700d66cdcdb54d70
wnrattweb <61352925+wnrattweb@users.noreply.github.com>
2020-09-17 02:13:29 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -144,4 +144,10 @@ $(window).on("load", function() {
 
 });
 
+$(window).on('load', function() {
+    var delayMs = 1500; // delay in milliseconds
 
+    setTimeout(function() {
+        $('#myModal').modal('show');
+    }, delayMs);
+});
